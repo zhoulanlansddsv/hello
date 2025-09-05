e29b73b078565d241ad68e2132c4c20ffb1c8381
blueblue_zhou <748578419@qq.com>
2025-09-05 18:16:57 +0800

    install_nmpy

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
 import torch
-print(torch.__version__)
+import numpy as np
+
+# 1D Tensor
+t1 = torch.tensor([1, 2, 3])
+print(t1)
+
+# 2D Tensor
+t2 = torch.tensor([[1, 2, 3], [4, 5, 6]])
+print(t2)
+
+# 3D Tensor
+t3 = torch.tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+print(t3)
+
+# 从 NumPy 创建 Tensor
+arr = np.array([1, 2, 3])
+t_np = torch.tensor(arr)
+print(t_np)
