9aca499535f3277f17915e3f6f8572f0268231d2
zhoulanlansddsv <748578419@qq.com>
2025-10-12 21:22:16 +0800

    洗牌发牌模拟
    play_cards洗牌发牌

---
+++ play_card.py
@@ -0,0 +1,146 @@
+# coding: utf-8
+from deck import DeckOfCards
+from deck import DeckOfCards
+from deck import DeckOfCards
+deck_of_cards=DeckOfCards()
+print(deck_of_cards)
+deck_of_cards.shuffle()
+print(deck_of_cards)
+deck_of_cards.deal_card()
+card=deck_of_cards.deal_card()
+str(card)
+card.image_name
+from deck import DeckOfCards
+deck_of_cards=DeckOfCards()
+get_ipython().run_line_magic('matplotlib', '')
+from pathlib import Path
+path=Path('E:/spider/PythonForProgrammers-master/example/ch10/').joinpath('card_images')
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+figure,axes_list=plt.subplots(nrows=4,ncols=13)
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+path=Path('E:\spider\PythonForProgrammers-master\example\ch10\').joinpath('card_images')
+path=Path('E:/spider/PythonForProgrammers-master/example/ch10/').joinpath('card_images')
+card.image_name=card.image_name.lower()
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name.lower()
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+for axes in axes_list.ravel():^M
+    ...:     axes.get_xaxis().set_visible(False)^M
+    ...:     axes.get_yaxis().set_visible(False)^M
+    ...:     image_name=deck_of_cards.deal_card().image_name.lower()^M
+    ...:     img=mpimg.imread(str(path.joinpath(image_name).resolve()))^M
+    ...:     axes.imshow(img)
+for axes in axes_list.ravel():^M
+    ...:     axes.get_xaxis().set_visible(False)^M
+    ...:     axes.get_yaxis().set_visible(False)^M
+    ...:     image_name=deck_of_cards.deal_card().image_name.lower()^M
+    ...:     img=mpimg.imread(str(path.joinpath(image_name).resolve()))^M
+    ...:     axes.imshow(img)
+path=Path('C:Users/Administrator').joinpath('card_images')
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name.lower()
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+path=Path('C:Users\Administrator').joinpath('card_images')
+for axes in axes_list.ravel():^M
+    ...:     axes.get_xaxis().set_visible(False)^M
+    ...:     axes.get_yaxis().set_visible(False)^M
+    ...:     image_name=deck_of_cards.deal_card().image_name.lower()^M
+    ...:     img=mpimg.imread(str(path.joinpath(image_name).resolve()))^M
+    ...:     axes.imshow(img)
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name.lower()
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+path=Path('.').joinpath('card_images')
+for axes in axes_list.ravel():^M
+    ...:     axes.get_xaxis().set_visible(False)^M
+    ...:     axes.get_yaxis().set_visible(False)^M
+    ...:     image_name=deck_of_cards.deal_card().image_name.lower()^M
+    ...:     img=mpimg.imread(str(path.joinpath(image_name).resolve()))^M
+    ...:     axes.imshow(img)
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name.lower()
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+    ...:     axes.imshow(img)
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name.lower()
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+deck_of_cards=DeckOfCards()
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name.lower()
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+for axes in axes_list.ravel():^M
+    ...:     axes.get_xaxis().set_visible(False)^M
+    ...:     axes.get_yaxis().set_visible(False)^M
+    ...:     image_name=deck_of_cards.deal_card().image_name^M
+    ...:     img=mpimg.imread(str(path.joinpath(image_name).resolve()))^M
+    ...:     axes.imshow(img)^M
+for axes in axes_listrravel():
+    ...:     axes.get_xaxis().set_visible(False)
+    ...:     axes.get_yaxis().set_visible(False)
+    ...:     image_name=deck_of_cards.deal_card）
+    ...:     img=mpimg.imread(str(path.joinpath(image_name).resolve())
+    ...:     axes.imshow(img)
+for axes in axes_listrravel():
+    ...:     ...:     axes.get_xaxis().set_visible(False)
+    ...:     ...:     axes.get_yaxis().set_visible(False)
+    ...:     ...:     image_name=deck_of_cards.deal_card
+    ...:     ...:     img=mpimg.imread(str(path.joinpath(image_name).resolve())
+    ...:     ...:     axes.imshow(img)
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+deck_of_cards=DeckOfCards()
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+figure.tight_layout()
+deck_of_cards.shuffle()
+ for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name=deck_of_cards.deal_card().image_name
+    img=mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+get_ipython().run_line_magic('save', 'play_card.py  24-72')
